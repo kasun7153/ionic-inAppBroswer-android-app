c510fdda59443a9432d0f8b4c41af6f4942a438a
kasun7153 <kasun7153@gmail.com>
2021-06-25 17:16:58 +0530

    before onesignal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser,public platform: Platform) {
+
+    this.platform.ready().then(()=>{
+      const browser = this.iab.create('https://proftac.com/',"_blank","location=no,zoom=no,hidden=yes");
+      browser.on("loadstop").subscribe((e)=>{
+        browser.show()
+      })
+      browser.on("exit").subscribe((e)=>{
+        navigator["app"].exitApp()
+      })
+    })
+
+  }
 
 }
